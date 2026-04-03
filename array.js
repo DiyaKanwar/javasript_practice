ff10d20fcbb5bd690e4ed29e865684988784e189
DiyaKanwar <kanwardiya1@gmail.com>
2026-04-03 16:25:06 +0530

    Add array manipulation examples and object structure in new file

--- array.js
+++ array.js
@@ -1,4 +1,5 @@
 const arr = [6,4,5,4,8];
+const num=3;
 //console.log(arr[1]);
 //arr.push(8);
 //console.log(arr);
@@ -11,12 +12,45 @@ const arr = [6,4,5,4,8];
 // console.log(arr.includes(20))
 // console.log(arr.sort())
 
-const arr2= arr.join(' ');
+// const arr2= arr.join(' ');
 // console.log(typeof(arr2))
 
 // console.log(arr2)
 // console.log(arr);
 // console.log(arr.slice(1,3))
-console.log(arr.splice(1,3))
+// console.log(arr.splice(1,3))
 
-console.log(arr);
+// const arr4=[4,5]
+// const arr2= [3,4,7,8];
+// const arr3=[...arr2,...arr4,...arr] // spread operator
+// console.log(arr3)
+// const nesarr=[2,3,4,5,[4,2],5,3,[7,4]]
+// console.log(nesarr)
+
+// const newarr= nesarr.flat(3)
+// console.log(newarr)
+// console.log(Array.isArray(num))
+// const new1=Array.from("Hello world")
+// console.log(new1)
+
+const n1=23
+const n2=24
+
+const n3=25
+// console.log(Array.of(n1,n2,n3))
+
+
+
+
+
+const arrn = Array.of(2,3,4)
+for (let index = 0; index < arrn.length; index++) {
+    arrn[index]=arrn[index]*2
+    
+}
+console.log(arrn)
+// console.log(typeof arrn)
+
+
+
+
---
+++ object.js
@@ -0,0 +1,76 @@
+const a = Symbol("A")
+/*
+const obj = {
+    username: "diya",
+    email: "diya1@gmail.com",
+    password: "***",
+    "full name": "Diya Kanwar",
+    bool: true,
+    [a]: "abc",
+
+    obj2:  {
+        hj: {
+            ab: "kjj",
+            kl: "oijj"
+        },
+        date: "5",
+        "first name": "Diya"
+    }
+}
+
+const obj3={
+    username: "Devashish",
+    id:122,
+    email:"1@gmail"
+}
+const obj4={
+    pass:"124",
+    id2:"22H"
+}
+const obj5={
+    obj3,
+    obj4
+} 
+
+// const newob= Object.assign({ },obj3,obj4)
+const newobject= {...obj3,...obj4}
+// console.log(newob)
+console.log(newobject)
+*/
+const arr=[
+     obj ={
+      password: "amazon",
+      email:"kanwar@gmail.com",  
+    },
+    obj2={
+        age:22
+    }
+]
+console.log(arr)
+// console.log(obj5)
+// console.log(obj)
+
+// console.log(obj.obj2.hj.ab)
+
+// console.log(typeof(obj.a))
+// console.log(obj[a])
+// console.log(obj)
+//console.log(obj.username)
+// console.log(obj["username"])
+// console.log(obj["full name"])
+
+
+
+// Object.freeze(obj)
+// obj.username="Diya"
+
+// obj.first="User"
+// console.log(obj.username)
+//console.log(obj)
+
+/* obj.greet = function(){
+  console.log("hello user")
+}  */
+
+//  console.log(typeof(obj))
+
